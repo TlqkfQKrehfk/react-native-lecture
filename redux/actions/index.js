f78f4cd9b152ed7262d6dcfdb57515b813b0e6a9
TlqkfQKrehfk <TlqkfQKrehfk@gmail.com>
2021-02-15 16:25:36 +0900

    'react-native-lecture'

--- redux/actions/index.js
+++ redux/actions/index.js
@@ -2,3 +2,7 @@ export const addAction = payload => ({
   type: 'ADD_ACTION',
   payload
 })
+export const removeAction = payload => ({
+  type: 'REMOVE_ACTION',
+  payload
+})
--- redux/reducers/actions.js
+++ redux/reducers/actions.js
@@ -2,28 +2,33 @@
 // Like 목록을 제어하는 리듀서
 // likes.js
 
-// 앤션 목록을 제어하는 리듀서
+// 액션 목록을 제어하는 리듀서
 // actions.js
 
 // 리듀서
-// (현재state, action의 리턴객체) => {
-//  switch....case .... action의 타입에 따라서 state를 제어함
-//  return 변경할 state
-//}
-const actions = (state = [], action) =>{
-  // action의 type별로 상태(state) 제어
-  switch (action.type){
+// (현재state, action의리턴객체) => {
+//   switch.... case.... action의 타입에 따라서 state르 제어  
+//   return 변경할state 
+// }
+
+const actions = (state = [], action) => {
+  // action의 type별로 state 제어
+  switch (action.type) {
     case 'ADD_ACTION':
-      // return 변경할 state
+      // return 변경할state
       // 현재 state를 복사하여 변경
       return [
-        // state배열의 요소들을 카피
-        // state == [{},{}] ...state -> {},{}
-        ...state,
-        // payload 객체 카피
+        // state 배열 요소들을 카피 
+        // state == [{}, {}] ...state -> {}, {}
+        ...state, 
+        // paylod 객체 카피
         {
           ...action.payload
         }
+      ]
+    case 'REMOVE_ACTION':
+      return [
+        ...state.filter(item => item.id != action.payload)
       ]
     default:
       return state
